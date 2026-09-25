d6fd61371c76dfdb381e2e06811daa1681e9ee90
Aya ِAdel <ayaadelyahoo66@gmail.com>
2026-09-25 22:58:53 +0300

    feat: complete register page  UI

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { RegisterFormComponent } from './features/auth/components/register-form/register-form.component';
+import { RegisterPageComponent } from './features/auth/pages/register-page/register-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RegisterFormComponent],
+  imports: [RouterOutlet, RegisterFormComponent, RegisterPageComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
