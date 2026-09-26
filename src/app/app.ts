cbf28b552181ab86e3502525e8b514f505ffd028
Aya ِAdel <ayaadelyahoo66@gmail.com>
2026-09-26 17:04:35 +0300

    feat: edit in design login form

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { RegisterFormComponent } from './features/auth/components/register-form/register-form.component';
 import { RegisterPageComponent } from './features/auth/pages/register-page/register-page.component';
+import { SignInFormComponent } from './features/auth/components/sign-in-form/sign-in-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RegisterFormComponent, RegisterPageComponent],
+  imports: [RouterOutlet, RegisterFormComponent, RegisterPageComponent, SignInFormComponent],
   templateUrl: './app.html',
 })
 export class App {
